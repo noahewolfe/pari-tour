d39a804b3934eeace8fe3882615866160ef614ee
Noah Wolfe <Noah Wolfe>
2016-07-27 22:11:14 -0400

    Basic ejs page with partials

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var app = express();
 var PORT = 8080;
 
 // set the view engine to ejs
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function (req, res) {
-    //res.render('pages/index');
-    res.send("Hello world!");
+    res.render('pages/index');
+    //res.send("Hello world!");
 });
 
 // listener
